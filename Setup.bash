89f15e6880ae176aa99772986be51a12ec10303e
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-10-17 17:20:58 +0000

    Chores : Update dependencies 4cicflowmeter

--- Setup.bash
+++ Setup.bash
@@ -1,9 +1,8 @@
-git clone https://github.com/datthinh1801/cicflowmeter.git
-cd cicflowmeter
+git clone https://gitlab.abo.fi/tahmad/cicflowmeter-py
+cd cicflowmeter-py
 
-pip install scipy==1.6.0
-pip install numpy==1.19.5
+sudo apt-get install python3-pip
 pip install -r requirements.txt
 sudo apt-get install python3-distutils
-
-python3 setup.py install
+# use "python3 setup.py install" if fails ( If not install make file)
+make install
