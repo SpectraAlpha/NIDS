63085bb153bb9f77cad6aa74d0e1112a2a2e8be0
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-10-17 16:48:33 +0000

    chore: Installation scripts

---
+++ Setup.bash
@@ -0,0 +1,9 @@
+git clone https://github.com/datthinh1801/cicflowmeter.git
+cd cicflowmeter
+
+pip install scipy==1.6.0
+pip install numpy==1.19.5
+pip install -r requirements.txt
+sudo apt-get install python3-distutils
+
+python3 setup.py install
